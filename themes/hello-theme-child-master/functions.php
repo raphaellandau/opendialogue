e9227dfd9ea8a0683176da0b06771212a90df239
deveagleray <58217866+deveagleray@users.noreply.github.com>
2026-03-19 13:54:38 +0200

    initial commit

---
+++ themes/hello-theme-child-master/functions.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementorChild
+ */
+
+/**
+ * Load child theme css and optional scripts
+ *
+ * @return void
+ */
+function hello_elementor_child_enqueue_scripts() {
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		'1.0.0'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+/* add remove cart buttons */
+
+function ns_woocommerce_checkout_remove_item( $product_name, $cart_item, $cart_item_key ) {
+if ( is_checkout() ) {
+    $_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+    $product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+
+    $remove_link = apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
+        '<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">×</a>',
+        esc_url( WC()->cart->get_remove_url( $cart_item_key ) ),
+        __( 'Remove this item', 'woocommerce' ),
+        esc_attr( $product_id ),
+        esc_attr( $_product->get_sku() )
+    ), $cart_item_key );
+
+    return '<span>' . $remove_link . '</span> <span>' . $product_name . '</span>';
+}
+
+return $product_name;
+}
+add_filter( 'woocommerce_cart_item_name', 'ns_woocommerce_checkout_remove_item', 10, 3 );
+
